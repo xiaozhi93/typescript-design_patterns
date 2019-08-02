64d83df456418bd37b317e3925737a780a7bb137
windfly <974589985@qq.com>
2019-08-02 18:30:27 +0800

    修改设计模式参考文档

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
   - [x] 抽象工厂模式
 - [ ] 结构型设计模式
 - [ ] 行为型设计模式
+
+## 参考文档
+- [图解设计模式](https://design-patterns.readthedocs.io/zh_CN/latest/index.html)
+- [设计模式Java版](https://quanke.gitbooks.io/design-pattern-java/%E8%AE%BE%E8%AE%A1%E6%A8%A1%E5%BC%8F%E6%A6%82%E8%BF%B0.html)
--- signleton/Signleton.ts
+++ signleton/Signleton.ts
@@ -1,21 +1,17 @@
 class Singleton {
-    constructor(data) {
-        if (Singleton.prototype.Instance === undefined) {
-            this.data = data;
-            Singleton.prototype.Instance = this;
-        }
-        return Singleton.prototype.Instance;
+  // 将constructor设为私有，防止通过new该类产生多个对象，破坏单例
+  private constructor() {}
+  static instance: Singleton;
+  // 如果已经有了对象，则返回缓存的对象，不然就创建一个对象并缓存，保证系统内最多只有一个该类的对象
+  static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
     }
+    return Singleton.instance;
+  }
 }
 
-let ob1 = new Singleton("one")
-let ob2 = new Singleton("two")
-let ob3 = new Singleton("Three")
-ob2.init = "Object flg"
 
-console.log(ob1 === ob2)
-console.log(ob1 === ob3)
+// let ob1 = new Singleton("one")
 
-console.log(ob1)
-console.log(ob2)
-console.log(ob3)
+// console.log(ob1)
